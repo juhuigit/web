1ec83034538864e9f2ee1a2cd4b8669a1b507df3
juhuigit <juhui.git@gmail.com>
2023-09-16 17:38:42 +0900

    week3 page move before

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+/* 라우터 등록*/
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var booksRouter = require('./routes/books');
@@ -20,6 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* 라우터 등록
+booksRouter(변수) 대신 require('./routes/books') 바로 대입해줘도 됨. */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/books', booksRouter);
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// render : 그려준다 라는 의미
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '안녕하세요!', pageName:'home.ejs'}); // view에 index.ejs 파일
+  res.render('index', { title: '홈페이지', pageName:'home.ejs'}); // view에 index.ejs 파일
 });
 
 module.exports = router;
